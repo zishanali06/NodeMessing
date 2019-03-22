09288ddfd7020e399212585d79632ffe590dca41
Zishan Ali <zishanali06@gmail.com>
2019-03-21 20:10:20 -0500

    messing around in NodeJS

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var fs = require('fs');
+
+let dpath = path.join(__dirname, "../data.json");
+
+console.log(dpath);
+
+fs.readFile(dpath,{
+    encoding: "UTF-8"
+}, (err, data) => {
+    var myinfo = JSON.parse(data);
+
+    console.log(myinfo);
+    console.log(`THIS IS WHERE I STAY: ${myinfo.location}`);
+})
